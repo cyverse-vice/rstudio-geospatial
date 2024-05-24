236c56932c039b3691c7582cc73906f299a96ebc
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2024-05-23 21:14:56 -0700

    Update run.sh

--- latest/run.sh
+++ latest/run.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
+touch $HOME/.irods/irods_environment.json
+echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' >> $HOME/.irods/irods_environment.json
+
+echo "export PATH=$PATH:/opt/conda/bin" >> ~/.bashrc
+
+if [ -f /data-store/iplant/home/$IPLANT_USER/.gitconfig ]; then
+  cp /data-store/iplant/home/$IPLANT_USER/.gitconfig ~/
+fi
+
+if [ -d /data-store/iplant/home/$IPLANT_USER/.ssh ]; then
+  cp -r /data-store/iplant/home/$IPLANT_USER/.ssh ~/
+fi
+
 gomplate -f /nginx.conf.tmpl -o /etc/nginx/nginx.conf
 supervisord -c /etc/supervisor/supervisord.conf -n
